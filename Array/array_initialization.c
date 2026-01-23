3629a93aa8b860ca9f60feb49eddf71b04622021
AhsanHabibHamim <coderahsan3.0@gmail.com>
2026-01-23 14:49:30 +0600

    New

--- Array/array_initialization.c
+++ Array/array_initialization.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    // int arr[5] = {1,2,3,4,5};
-    int arr[10] = {1,2,3,4,5};
+    int arr[5] = {1,2,3,4,5};
+    // int arr[10] = {1,2,3,4,5};
     for(int i = 0; i<10; i++)
     {
         printf("%d\n", arr[i]);
