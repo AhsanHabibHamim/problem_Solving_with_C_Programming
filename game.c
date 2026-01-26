50e15a12db35e9413a68d2b5ae1f090fd9865227
AhsanHabibHamim <coderahsan3.0@gmail.com>
2026-01-26 17:10:23 +0600

    game

---
+++ game.c
@@ -0,0 +1,59 @@
+#include <SDL.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Event event;
+
+    int running = 1;
+    int x = 300, y = 200;
+    int speed = 5;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow(
+        "My First Visual C Game",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        0
+    );
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    while (running) {
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
+                running = 0;
+        }
+
+        const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+        if (keys[SDL_SCANCODE_UP]) y -= speed;
+        if (keys[SDL_SCANCODE_DOWN]) y += speed;
+        if (keys[SDL_SCANCODE_LEFT]) x -= speed;
+        if (keys[SDL_SCANCODE_RIGHT]) x += speed;
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_Rect player = { x, y, 40, 40 };
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &player);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
