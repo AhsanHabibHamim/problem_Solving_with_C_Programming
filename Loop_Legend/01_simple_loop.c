7b47ad6398edbe0b6d04d7790342134d80b01ad7
AhsanHabibHamim <coderahsan3.0@gmail.com>
2026-02-02 18:45:40 +0600

    practice_Wise

---
+++ Loop_Legend/01_simple_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    // Write I Love You For 100 Times using for loop
+    for (int i = 1; i < 101; i++)
+    {
+        printf("%d . I Love You \n", i);
+    }
+
+    return 0;
+}
