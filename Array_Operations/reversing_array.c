ba03bba91b365a7f027b1c63ab68a542b18c495f
AhsanHabibHamim <coderahsan3.0@gmail.com>
2026-02-19 16:30:20 +0600

    ready'

--- Array_Operations/reversing_array.c
+++ Array_Operations/reversing_array.c
@@ -2,21 +2,22 @@
 int main()
 {
     int n;
-    printf("Enter the size of Array: ");
     scanf("%d", &n);
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    int x;
-    printf("Enter the index whose value you want to delete: ");
-    scanf("%d", &x);
-    for (int i = x; i < n - 1; i++)
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
     {
-        arr[i] = arr[i + 1];
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        i++;
+        j--;
     }
-    n--;
     for (int i = 0; i < n; i++)
     {
         printf("%d \n", arr[i]);
