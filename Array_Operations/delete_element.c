bd273e98de9349e4fe5585dfb5880933af188a74
AhsanHabibHamim <coderahsan3.0@gmail.com>
2026-02-19 14:05:26 +0600

    solved deleted

--- Array_Operations/delete_element.c
+++ Array_Operations/delete_element.c
@@ -9,7 +9,14 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < n; i++)
+    int x;
+    printf("Enter the index whose value you want to delete: ");
+    scanf("%d", &x);
+    for (int i = x; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    for (int i = 0; i < n-1; i++)
     {
         printf("%d \n", arr[i]);
     }
